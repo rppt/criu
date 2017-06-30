4b0e2d48b82227af6285d7bb144660d07be296fc
Pavel Emelyanov <xemul@virtuozzo.com>
2017-06-30 14:02:54 +0300

    files: Move pipes into files image
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -261,7 +261,6 @@ static int crtools_prepare_shared(void)
  */
 
 static struct collect_image_info *cinfos[] = {
-	&pipe_cinfo,
 	&file_locks_cinfo,
 	&pipe_data_cinfo,
 	&fifo_data_cinfo,
@@ -271,6 +270,7 @@ static struct collect_image_info *cinfos[] = {
 static struct collect_image_info *cinfos_files[] = {
 	&unix_sk_cinfo,
 	&fifo_cinfo,
+	&pipe_cinfo,
 	&nsfile_cinfo,
 	&packet_sk_cinfo,
 	&netlink_sk_cinfo,
