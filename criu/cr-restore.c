cd991f063b7f5e9ba6b20fe7a6ac6f26ae7a37d9
Pavel Emelyanov <xemul@virtuozzo.com>
2017-06-30 14:01:47 +0300

    files: Move fanotify into files image
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -264,8 +264,6 @@ static struct collect_image_info *cinfos[] = {
 	&pipe_cinfo,
 	&fifo_cinfo,
 	&unix_sk_cinfo,
-	&fanotify_cinfo,
-	&fanotify_mark_cinfo,
 	&ext_file_cinfo,
 	&file_locks_cinfo,
 	&pipe_data_cinfo,
@@ -285,6 +283,8 @@ static struct collect_image_info *cinfos_files[] = {
 	&timerfd_cinfo,
 	&inotify_cinfo,
 	&inotify_mark_cinfo,
+	&fanotify_cinfo,
+	&fanotify_mark_cinfo,
 };
 
 /* These images are requered to restore namespaces */
