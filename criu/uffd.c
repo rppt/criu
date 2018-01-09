1d313851ac45e0f4ab8c65100560c6455a1f1846
Mike Rapoport <rppt@linux.vnet.ibm.com>
2018-01-09 11:40:32 +0200

    lazy-pages: warn when getting page fault with empty VMAs list
    Signed-off-by: Mike Rapoport <rppt@linux.vnet.ibm.com>

--- criu/uffd.c
+++ criu/uffd.c
@@ -1299,8 +1299,10 @@ static int handle_page_fault(struct lazy_pages_info *lpi, struct uffd_msg *msg)
 	if (is_page_queued(lpi, address))
 		return 0;
 
-	if (list_empty(&lpi->vmas))
+	if (list_empty(&lpi->vmas)) {
+		lp_warn(lpi, "spurious page fault\n");
 		return 0;
+	}
 
 	iov = find_iov(lpi, address);
 	if (!iov)
