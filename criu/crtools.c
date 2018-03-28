abce487db0a9867802c8c58d7779d92078ba67cd
Dmitry Safonov <dima@arista.com>
2018-03-28 17:19:45 +0100

    criu: Remove PAGE_IMAGE_SIZE
    It's unused since commit fd3f33f5d23a ("headers: image.h -- Drop unused
    entries"), so let's remove it completely.
    
    Signed-off-by: Dmitry Safonov <dima@arista.com>

--- criu/crtools.c
+++ criu/crtools.c
@@ -522,7 +522,6 @@ int main(int argc, char *argv[], char *envp[])
 
 #undef BOOL_OPT
 
-	BUILD_BUG_ON(PAGE_SIZE != PAGE_IMAGE_SIZE);
 	BUILD_BUG_ON(CTL_32 != SYSCTL_TYPE__CTL_32);
 	BUILD_BUG_ON(__CTL_STR != SYSCTL_TYPE__CTL_STR);
 
